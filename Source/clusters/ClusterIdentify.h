2dbd974f3ab8d64f04faba196d07d5ee1fc2a52b
Paolo Achdjian <pachdjian@gmail.com>
2016-08-24 15:33:45 +0200

    fix led output

---
+++ Source/clusters/ClusterIdentify.h
@@ -0,0 +1,29 @@
+/**************************************************************************************************
+  Filename:       ClusterIdentify.h
+
+  Autorh:  Paolo Achdjia
+  Created: 28/10/2014
+
+**************************************************************************************************/
+
+#ifndef __CLUSTER_IDENTIFY__H_
+#define __CLUSTER_IDENTIFY__H_
+
+#include "zcl.h"
+#include "zclReadAttributeFn.h"
+#include "zclWriteAttributeFn.h"
+
+
+void identifyInit(byte taskId);
+void processIdentifyTimeChange( void );
+uint16 identifyLoop(uint16 events);
+void fastBlinkOn(void);
+void fastBlinkOff(void);
+
+ZStatus_t processIdentifyClusterServerCommands( zclIncoming_t *pInMsg );
+ZStatus_t processIdentifyClusterClientCommands( zclIncoming_t *pInMsg );
+
+void identifyClusterReadAttribute(zclAttrRec_t *);
+void identifyClusterWriteAttribute(ZclWriteAttribute_t *);
+
+#endif
